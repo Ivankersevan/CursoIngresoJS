55287d53ddcdda5091b0930004d7c42f952cdf63
Ivan Kersevan <40776546+Ivankersevan@users.noreply.github.com>
2018-07-03 14:33:18 -0300

    actividad 6 en casa

---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,31 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+mostrar el resulto por medio de "ALERT"*/
+function sumar()
+{
+   
+var num1;
+var num2;
+var num3;
+
+num1 = prompt("Ingrese su numero a sumar");
+num2 = prompt("Ingrese su segundo numero a sumar");
+
+
+
+document.getElementById('numeroUno').value = num1;
+document.getElementById('numeroDos').value = num2;
+
+document.getElementByClick('numeroUno'+'numeroDos').value = num3;
+
+alert(num3);
+
+
+
+
+
+
+
+
+}
+
