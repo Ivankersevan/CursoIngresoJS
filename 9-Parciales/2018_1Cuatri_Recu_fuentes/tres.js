c8afb116b90dce869842376493596d0fb776a84c
Ivan Kersevan <40776546+Ivankersevan@users.noreply.github.com>
2018-07-05 21:57:21 -0300

    Recuperatorio actividad en clase

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,34 @@
 function mostrar()
 {
 
+var precio;
+var descuento;
+var Pdescuento;
+var descuentoDeDinero;
+var iva;
+var total;
+
+precio = prompt("Ingrese el precio: ");
+descuento = prompt("Ingrese el descuento");
+precio=parseInt(precio);
+descuento=parseInt(descuento);
+
+Pdescuento = (precio * descuento) / 100;
+
+descuentoDeDinero = Pdescuento-precio;
+
+iva = (precio / 100) * 21;
+
+total = Pdescuento + iva;
+
+
+
+
+
+alert("El descuento total segun el dinero es: "+descuentoDeDinero+ "$ El descuento en dinero es: "+Pdescuento+"$ El IVA es: "+iva"$");
+
+document.getElementById('elPrecioFinal').value = total;
+
+
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -2,4 +2,28 @@
 function mostrar()
 {
 
+var base;
+var altura;
+var superficie;
+var hipotenisa;
+var perimetro;
+
+base = prompt("Ingrese la base del triangulo");
+altura = prompt("Ingrese la altura del triangulo");
+
+superficie = (base * altura) / 2;
+hipotenusa = (base*base) + (altura*altura);
+hipotenusa = Math.sqrt (hipotenusa);
+perimetro = hipotenusa + superficie + altura;
+
+alert("La superficie y el perimetro del triangulo es: "+superficie+","+perimetro);
+
+
+
+
+
+
+
+
+
 }
