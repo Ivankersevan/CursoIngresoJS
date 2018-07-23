d4e427379ece1ac3e8eb7a35c8a881e93be472f1
Ivan Kersevan <40776546+Ivankersevan@users.noreply.github.com>
2018-07-23 20:06:32 -0300

    Add files via upload

---
+++ jsPiedarPapelTijera2.js
@@ -0,0 +1,69 @@
+var eleccionMaquina;
+var ContadorDeEmpates=0;
+var ContadorDeGanadas=0;
+var ContadorDePerdidas=0;
+
+function comenzar()
+{
+	eleccionMaquina=Math.floor((Math.random()*3)-1);//Genero el número RANDOM entre 1 y 3
+	 	
+
+
+
+}//FIN DE LA FUNCIÓN
+function piedra()
+{
+	var piedra=1;
+	if (eleccionMaquina == piedra) {
+
+  ContadorDeEmpates ++;
+   alert("Empate");
+	}else if (eleccionMaquina == 2) {
+  ContadorDePerdidas ++;
+ alert("Perdiste");
+	}else if (eleccionMaquina == 3) {
+  ContadorDeGanadas ++;
+ alert("Ganaste");
+
+	}
+
+}//FIN DE LA FUNCIÓN
+function papel()
+{
+var papel=2;
+if (eleccionMaquina == papel) {
+ContadorDeEmpates ++;
+alert("Empate");
+}else if (eleccionMaquina == 1) {
+	ContadorDeGanadas ++;
+	alert("Ganaste");
+}else if (eleccionMaquina == 3) {
+	ContadorDePerdidas ++;
+	alert("Perdiste");
+}
+
+
+
+
+
+}//FIN DE LA FUNCIÓN
+function tijera()
+{
+	var tijera=3;
+if (eleccionMaquina == tijera) {
+	ContadorDeEmpates ++;
+	alert("Empate");
+}else if (eleccionMaquina == 1) {
+	ContadorDePerdidas ++;
+	alert("Perdiste");
+}else if (eleccionMaquina == 2) {
+	ContadorDeGanadas ++;
+	alert("Ganaste");
+}
+
+}//FIN DE LA FUNCIÓN
+
+function mostrarResultado()
+{
+comenzar();
+}
